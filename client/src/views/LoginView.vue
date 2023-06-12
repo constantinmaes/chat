b850c809671fe8656cbe4ca9f37aecb4fa20d89a
Maes Constantin <constantinmaes@gmail.com>
2023-06-12 18:32:27 +0200

    feat(client): add login

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,7 +1,25 @@
 <template>
     <div>
         <h1>Login</h1>
+        <div>Qui êtes-vous ?</div>
+        <div>
+            <input type="text" placeholder="Votre nom" v-model="myName" />
+        </div>
+        <div>
+            <button @click="login">Login</button>
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const myName = ref('');
+
+const login = () => {
+    console.log('Je suis la fonction login', myName.value);
+    localStorage.setItem('myName', myName.value);
+};
+</script>
 
 <style></style>
