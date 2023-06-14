12ae55d7d30af0f9e9e201efa103f93eebc5c5ed
Maes Constantin <constantinmaes@gmail.com>
2023-06-14 17:49:36 +0200

    feat(server): add rooms

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import * as crypto from 'crypto';
 
+const rooms = [];
+
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
@@ -15,6 +17,14 @@ io.on('connection', (socket) => {
         `Connection detected with id ${socket.id} from user ${socket.handshake.auth.name}`
     );
 
+    const participateIn = rooms.find((r) =>
+        r.includes(socket.handshake.auth.name.toLowerCase())
+    );
+
+    if (participateIn) {
+        participateIn.forEach((r) => socket.join(r));
+    }
+
     // ecoute l'événement de déconnexion
     socket.on('disconnect', (reason) =>
         console.log(`User ${socket.handshake.auth.name} disconnected`)
@@ -26,6 +36,19 @@ io.on('connection', (socket) => {
         console.log(
             `Hello from ${socket.handshake.auth.name} with data ${str}`
         );
+    });
+
+    // écoute l'événement ask-to-join-room
+    socket.on('ask-to-join-room', (n) => {
+        console.log(
+            `${socket.handshake.auth.name} asked to join room with ${n}`
+        );
+        const names = [socket.handshake.auth.name, n];
+        names.sort();
+        const roomName = names.join('-').toLowerCase();
+        rooms.push(roomName);
+        console.log(roomName);
+        socket.join(roomName);
     });
 });
 
